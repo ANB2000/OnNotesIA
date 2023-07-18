635d8455679efa2e5b3d2827a0d42cc45c0d10a3
ANB2000 <navaarmando.ags247@gmail.com>
2023-07-17 21:42:49 -0600

    Se agregan cambios ligeros que no afectan el funcionamiento

--- agenda/urls.py
+++ agenda/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from notesapp.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
