669301f2228b28e908f1405fec32228c67efdba0
ANB2000 <navaarmando.ags247@gmail.com>
2023-07-26 18:54:56 -0600

    Se creo y elimino doc html para pruebas no afecta en el funcionamiento

--- notesapp/urls.py
+++ notesapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . views import home, comida, principal
 
+
 urlpatterns = [
     path('', home, name='index'),
     path('registro/', comida, name='comida'),
     path('principal/', principal, name='principal'),
+    
 ]
